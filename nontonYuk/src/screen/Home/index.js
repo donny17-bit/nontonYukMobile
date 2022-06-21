d16aacfea4864fc3d7fd19c97221311a0b422d8a
Donny <doniwahyu14@gmail.com>
2022-06-21 15:43:57 +0700

    integration now showing home

--- nontonYuk/src/screen/Home/index.js
+++ nontonYuk/src/screen/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
@@ -9,262 +9,301 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Button,
+  FlatList,
+  ActivityIndicator,
 } from 'react-native';
 import {StyleSheet} from 'react-native';
+import axios from '../../utils/axios';
 
 function HomeScreen(props) {
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
 
+  const [data, setData] = useState([]);
+
   const [text, onChangeText] = React.useState(null);
+
+  const getMovies = async () => {
+    try {
+      const result = await axios.get('movie?limit=10&page=1');
+      setData(result.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderItem = ({item}) => (
+    <View style={{flexDirection: 'row', paddingTop: 20}}>
+      <View style={styles.imageBorder}>
+        <Image
+          source={{
+            uri: `https://res.cloudinary.com/dusoicuhh/image/upload/v1652761552/${item.image}`,
+          }}
+          style={{
+            height: 200,
+            resizeMode: 'cover',
+          }}
+        />
+      </View>
+    </View>
+  );
 
   const handleViewAll = () => {
     props.navigation.navigate('ListMovie');
   };
 
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Text style={{color: '#A0A3BD', paddingTop: 30, fontSize: 14}}>
-          Nearest Cinema, Newest Movie,
-        </Text>
-        <Text style={styles.title}>Find out now!</Text>
-      </View>
-      <View
-        style={{backgroundColor: 'white', paddingTop: 50, paddingBottom: 100}}>
-        <Image
-          style={{
-            height: 400,
-            width: windowWidth,
-          }}
-          source={require('../../assets/Group-13.png')}
-        />
-      </View>
-      <View style={styles.showing}>
-        <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
-          <Text
-            style={{
-              color: '#752EEA',
-              fontSize: 18,
-              fontWeight: '700',
-            }}>
-            Now Showing
+    <View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Text style={{color: '#A0A3BD', paddingTop: 30, fontSize: 14}}>
+            Nearest Cinema, Newest Movie,
           </Text>
-          <TouchableOpacity onPress={handleViewAll}>
+          <Text style={styles.title}>Find out now!</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: 'white',
+            paddingTop: 50,
+            paddingBottom: 100,
+          }}>
+          <Image
+            style={{
+              height: 400,
+              width: windowWidth,
+            }}
+            source={require('../../assets/Group-13.png')}
+          />
+        </View>
+        <View style={styles.showing}>
+          <View style={{justifyContent: 'space-between', flexDirection: 'row'}}>
             <Text
               style={{
                 color: '#752EEA',
-                fontSize: 14,
-                fontWeight: '600',
+                fontSize: 18,
+                fontWeight: '700',
               }}>
-              view all
+              Now Showing
             </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <View style={styles.imageBorder}>
-            <Image source={require('../../assets/Rectangle-119.png')} />
+            <TouchableOpacity onPress={handleViewAll}>
+              <Text
+                style={{
+                  color: '#752EEA',
+                  fontSize: 14,
+                  fontWeight: '600',
+                }}>
+                view all
+              </Text>
+            </TouchableOpacity>
           </View>
-          <View style={styles.imageBorder}>
-            <Image source={require('../../assets/Rectangle-119.png')} />
+          <FlatList
+            data={data}
+            horizontal={true}
+            keyExtractor={item => item.id}
+            style={{paddingBottom: 20}}
+            renderItem={renderItem}
+          />
+        </View>
+        <View style={styles.container}>
+          <View
+            style={{
+              paddingTop: 50,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <Text style={{fontSize: 18, fontWeight: '700', color: 'black'}}>
+              Upcoming Movies
+            </Text>
+            <TouchableOpacity onPress={handleViewAll}>
+              <Text style={{fontSize: 14, fontWeight: '600', color: '#5F2EEA'}}>
+                view all
+              </Text>
+            </TouchableOpacity>
           </View>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <View
-          style={{
-            paddingTop: 50,
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-          }}>
-          <Text style={{fontSize: 18, fontWeight: '700', color: 'black'}}>
-            Upcoming Movies
-          </Text>
-          <TouchableOpacity onPress={handleViewAll}>
-            <Text style={{fontSize: 14, fontWeight: '600', color: '#5F2EEA'}}>
-              view all
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{top: 20, flexDirection: 'row'}}>
-          <Button title="September" color="#5F2EEA" style={{flex: 2}} />
-        </View>
-        <View style={{flexDirection: 'row', top: 60, paddingBottom: 100}}>
-          <View style={styles.imageCard}>
-            <Image source={require('../../assets/Rectangle-119.png')} />
-            <Text
-              style={{
-                paddingTop: 10,
-                color: 'black',
-                fontSize: 14,
-                fontWeight: '600',
-              }}>
-              Spider-man
-            </Text>
-            <Text
-              style={{
-                paddingTop: 10,
-                color: '#A0A3BD',
-                fontSize: 11,
-                fontWeight: '300',
-              }}>
-              Action, Adventure, Sci-Fi
-            </Text>
-            <View style={{paddingTop: 20}}>
-              <Button title="Details" style={{borderColor: '#5F2EEA'}} />
+          <View style={{top: 20, flexDirection: 'row'}}>
+            <Button title="September" color="#5F2EEA" style={{flex: 2}} />
+          </View>
+          <View style={{flexDirection: 'row', top: 60, paddingBottom: 100}}>
+            <View style={styles.imageCard}>
+              <Image source={require('../../assets/Rectangle-119.png')} />
+              <Text
+                style={{
+                  paddingTop: 10,
+                  color: 'black',
+                  fontSize: 14,
+                  fontWeight: '600',
+                }}>
+                Spider-man
+              </Text>
+              <Text
+                style={{
+                  paddingTop: 10,
+                  color: '#A0A3BD',
+                  fontSize: 11,
+                  fontWeight: '300',
+                }}>
+                Action, Adventure, Sci-Fi
+              </Text>
+              <View style={{paddingTop: 20}}>
+                <Button title="Details" style={{borderColor: '#5F2EEA'}} />
+              </View>
+            </View>
+            <View style={styles.imageCard}>
+              <Image source={require('../../assets/Rectangle-119.png')} />
+              <Text
+                style={{
+                  paddingTop: 10,
+                  color: 'black',
+                  fontSize: 14,
+                  fontWeight: '600',
+                }}>
+                Spider-man
+              </Text>
+              <Text
+                style={{
+                  paddingTop: 10,
+                  color: '#A0A3BD',
+                  fontSize: 11,
+                  fontWeight: '300',
+                }}>
+                Action, Adventure, Sci-Fi
+              </Text>
+              <View style={{paddingTop: 20}}>
+                <Button title="Details" style={{borderColor: '#5F2EEA'}} />
+              </View>
             </View>
           </View>
-          <View style={styles.imageCard}>
-            <Image source={require('../../assets/Rectangle-119.png')} />
-            <Text
-              style={{
-                paddingTop: 10,
-                color: 'black',
-                fontSize: 14,
-                fontWeight: '600',
-              }}>
-              Spider-man
-            </Text>
-            <Text
-              style={{
-                paddingTop: 10,
-                color: '#A0A3BD',
-                fontSize: 11,
-                fontWeight: '300',
-              }}>
-              Action, Adventure, Sci-Fi
-            </Text>
+          <View style={{paddingTop: 100}}>
+            <View style={{justifyContent: 'center', flexDirection: 'row'}}>
+              <Text style={{color: '#4E4B66', fontSize: 14, fontWeight: '400'}}>
+                Be the vanguard of the
+              </Text>
+            </View>
+            <View style={{justifyContent: 'center', flexDirection: 'row'}}>
+              <Text style={{color: '#5F2EEA', fontSize: 32, fontWeight: '700'}}>
+                Moviegoers
+              </Text>
+            </View>
+            <SafeAreaView style={{paddingTop: 30}}>
+              <TextInput
+                style={styles.input}
+                // onChangeText={onChangeText}
+                value={text}
+              />
+            </SafeAreaView>
             <View style={{paddingTop: 20}}>
-              <Button title="Details" style={{borderColor: '#5F2EEA'}} />
+              <Button title="Join now" color="#5F2EEA" style={{flex: 1}} />
+            </View>
+            <View style={{alignItems: 'center', padding: 40}}>
+              <Text style={styles.commonText}>
+                By joining you as a Tickitz member,
+              </Text>
+              <Text style={styles.commonText}>we will always send you the</Text>
+              <Text style={styles.commonText}>latest updates via email .</Text>
             </View>
           </View>
         </View>
-        <View style={{paddingTop: 100}}>
-          <View style={{justifyContent: 'center', flexDirection: 'row'}}>
-            <Text style={{color: '#4E4B66', fontSize: 14, fontWeight: '400'}}>
-              Be the vanguard of the
-            </Text>
-          </View>
-          <View style={{justifyContent: 'center', flexDirection: 'row'}}>
-            <Text style={{color: '#5F2EEA', fontSize: 32, fontWeight: '700'}}>
-              Moviegoers
-            </Text>
-          </View>
-          <SafeAreaView style={{paddingTop: 30}}>
-            <TextInput
-              style={styles.input}
-              // onChangeText={onChangeText}
-              value={text}
-            />
-          </SafeAreaView>
-          <View style={{paddingTop: 20}}>
-            <Button title="Join now" color="#5F2EEA" style={{flex: 1}} />
-          </View>
-          <View style={{alignItems: 'center', padding: 40}}>
+        {/* footer */}
+        <View style={styles.footer}>
+          <Image source={require('../../assets/Tickitz-2.png')} />
+          <View style={{paddingTop: 20, paddingBottom: 40}}>
             <Text style={styles.commonText}>
-              By joining you as a Tickitz member,
+              Stop waiting in line. Buy tickets
             </Text>
-            <Text style={styles.commonText}>we will always send you the</Text>
-            <Text style={styles.commonText}>latest updates via email .</Text>
+            <Text style={styles.commonText}>
+              conveniently, watch movies quietly.
+            </Text>
           </View>
-        </View>
-      </View>
-      {/* footer */}
-      <View style={styles.footer}>
-        <Image source={require('../../assets/Tickitz-2.png')} />
-        <View style={{paddingTop: 20, paddingBottom: 40}}>
-          <Text style={styles.commonText}>
-            Stop waiting in line. Buy tickets
+          <Text style={{color: 'black', fontSize: 16, fontWeight: '600'}}>
+            Explore
           </Text>
-          <Text style={styles.commonText}>
-            conveniently, watch movies quietly.
-          </Text>
-        </View>
-        <Text style={{color: 'black', fontSize: 16, fontWeight: '600'}}>
-          Explore
-        </Text>
-        {/* link */}
-        <View style={{paddingTop: 20, flexDirection: 'row'}}>
+          {/* link */}
+          <View style={{paddingTop: 20, flexDirection: 'row'}}>
+            <Text
+              style={{
+                flex: 1,
+                color: '#6E7191',
+                fontSize: 14,
+                fontWeight: '400',
+              }}>
+              Home
+            </Text>
+            <Text
+              style={{
+                flex: 2,
+                color: '#6E7191',
+                fontSize: 14,
+                fontWeight: '400',
+              }}>
+              List Movie
+            </Text>
+          </View>
           <Text
             style={{
-              flex: 1,
-              color: '#6E7191',
-              fontSize: 14,
-              fontWeight: '400',
+              paddingTop: 40,
+              color: 'black',
+              fontSize: 16,
+              fontWeight: '600',
             }}>
-            Home
+            Our Sponsor
           </Text>
+          <View style={{flexDirection: 'row', paddingTop: 20}}>
+            <Image
+              source={require('../../assets/ebv.id.png')}
+              style={{marginRight: 20}}
+            />
+            <Image
+              source={require('../../assets/CineOne21.png')}
+              style={{marginRight: 20, marginTop: 10}}
+            />
+            <Image
+              source={require('../../assets/hiflix.png')}
+              style={{marginRight: 20}}
+            />
+          </View>
           <Text
             style={{
-              flex: 2,
-              color: '#6E7191',
-              fontSize: 14,
-              fontWeight: '400',
+              paddingTop: 50,
+              color: 'black',
+              fontSize: 16,
+              fontWeight: '600',
             }}>
-            List Movie
+            Follow us
+          </Text>
+          <View style={{paddingTop: 20, flexDirection: 'row'}}>
+            <Image
+              source={require('../../assets/facebook.png')}
+              style={{marginRight: 20}}
+            />
+            <Image
+              source={require('../../assets/instagram.png')}
+              style={{marginRight: 20}}
+            />
+            <Image
+              source={require('../../assets/twitter.png')}
+              style={{marginRight: 20}}
+            />
+            <Image
+              source={require('../../assets/youtube.png')}
+              style={{marginRight: 20}}
+            />
+          </View>
+          <Text
+            style={{
+              color: '#6E7191',
+              fontSize: 13,
+              fontWeight: '400',
+              paddingTop: 50,
+            }}>
+            © 2020 Tickitz. All Rights Reserved.
           </Text>
         </View>
-        <Text
-          style={{
-            paddingTop: 40,
-            color: 'black',
-            fontSize: 16,
-            fontWeight: '600',
-          }}>
-          Our Sponsor
-        </Text>
-        <View style={{flexDirection: 'row', paddingTop: 20}}>
-          <Image
-            source={require('../../assets/ebv.id.png')}
-            style={{marginRight: 20}}
-          />
-          <Image
-            source={require('../../assets/CineOne21.png')}
-            style={{marginRight: 20, marginTop: 10}}
-          />
-          <Image
-            source={require('../../assets/hiflix.png')}
-            style={{marginRight: 20}}
-          />
-        </View>
-        <Text
-          style={{
-            paddingTop: 50,
-            color: 'black',
-            fontSize: 16,
-            fontWeight: '600',
-          }}>
-          Follow us
-        </Text>
-        <View style={{paddingTop: 20, flexDirection: 'row'}}>
-          <Image
-            source={require('../../assets/facebook.png')}
-            style={{marginRight: 20}}
-          />
-          <Image
-            source={require('../../assets/instagram.png')}
-            style={{marginRight: 20}}
-          />
-          <Image
-            source={require('../../assets/twitter.png')}
-            style={{marginRight: 20}}
-          />
-          <Image
-            source={require('../../assets/youtube.png')}
-            style={{marginRight: 20}}
-          />
-        </View>
-        <Text
-          style={{
-            color: '#6E7191',
-            fontSize: 13,
-            fontWeight: '400',
-            paddingTop: 50,
-          }}>
-          © 2020 Tickitz. All Rights Reserved.
-        </Text>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 }
 
@@ -296,7 +335,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: 'white',
     borderRadius: 6,
-    top: 20,
     width: 145,
     marginEnd: 20,
   },
